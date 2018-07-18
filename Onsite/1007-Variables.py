f78e04aaf7f13965fb09867ae9d4cfa48dded90e
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2018-07-18 21:32:48 +0700

    ✨ Added Onsite-Week-1-1 Codes

---
+++ Onsite/1007-Variables.py
@@ -0,0 +1,11 @@
+"""
+Pre-Programming 61 Solution
+By Teerapat Kraisrisirikul
+"""
+
+def main():
+    """ Main function """
+    times, result, team = int(input()), input(), input()
+    print(team, 'has', result, times, 'times in a row.')
+
+main()
---
+++ Onsite/1008-Username.py
@@ -0,0 +1,11 @@
+"""
+Pre-Programming 61 Solution
+By Teerapat Kraisrisirikul
+"""
+
+def main():
+    """ Main function """
+    extra, name = input(), input()
+    print(name + extra)
+
+main()
