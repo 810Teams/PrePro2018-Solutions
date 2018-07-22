754de1c8ee0c4359c9af5728b016f8b142470746
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2018-07-22 19:30:55 +0700

    :art: Minor Code Refactoring (1130)

--- Onsite/1130-Matrix Multiplication 3x3.py
+++ Onsite/1130-Matrix Multiplication 3x3.py
@@ -5,11 +5,9 @@ By Teerapat Kraisrisirikul
 
 def main():
     """ Main function """
-    size_p, size_q, size_r = 3, 3, 3
-
-    matrix_a = [[int(input()) for _ in range(size_q)] for _ in range(size_p)]
-    matrix_b = [[int(input()) for _ in range(size_r)] for _ in range(size_q)]
-    matrix_r = [[multiply(matrix_a, matrix_b, i, j) for j in range(size_r)] for i in range(size_p)]
+    matrix_a = [[int(input()) for _ in range(3)] for _ in range(3)]
+    matrix_b = [[int(input()) for _ in range(3)] for _ in range(3)]
+    matrix_r = [[multiply(matrix_a, matrix_b, i, j) for j in range(3)] for i in range(3)]
 
     for i in matrix_r:
         for j in i:
@@ -20,6 +18,7 @@ def multiply(matrix_a, matrix_b, select_row_a, select_col_b):
     """ Multiply a row from matrix_a with a column from matrix_b """
     row = matrix_a[select_row_a]
     col = [i[select_col_b] for i in matrix_b]
+
     return sum([row[i]*col[i] for i in range(len(row))])
 
 main()
